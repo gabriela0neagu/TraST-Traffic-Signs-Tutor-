05dfb3a6131b90b2c632d6c466196eb44197ea1c
teodoratrinca <61377986+teodoratrinca@users.noreply.github.com>
2020-05-29 20:35:29 +0300

    Meniu
    Adaugarea legaturilor corecte in meniu si , adaugarea noului meniu in paginile lipsa

--- php/index.php
+++ php/index.php
@@ -26,19 +26,27 @@ $database = new CreateDb( "tw", "indicatoare_si_marcaje");
 
     <!-- Item 1 -->
     <input type="radio" name="slideItem" id="slide-item-1" class="slide-toggle" checked/>
-    <label for="slide-item-1"><p class="icon">★</p><span><a href="index.html">Categorii permis </a></span></label>
+  <label for="slide-item-1"><p class="icon">★</p><span><a href="../html/Trast.html">Acasa</a></span></label>
+  
+  <!-- Item 2 -->
+  <input type="radio" name="slideItem" id="slide-item-2" class="slide-toggle"/>
+  <label for="slide-item-2"><p class="icon">★</p><span><a href="../html/curslegislatie1.html">Curs de legislatie </a></span></label>
 
-    <!-- Item 2 -->
-    <input type="radio" name="slideItem" id="slide-item-2" class="slide-toggle"/>
-    <label for="slide-item-2"><p class="icon">★</p><span><a href="http://localhost/tw/curslegislatie1.html">Curs de legislatie </a></span></label>
+<!-- Item 3 -->
+  <input type="radio" name="slideItem" id="slide-item-3" class="slide-toggle"/>
+  <label for="slide-item-3"><p class="icon">★</p><span><a href="../php/index.php">Indicatoare si marcaje</a></span></label>
 
-    <!-- Item 3 -->
-    <input type="radio" name="slideItem" id="slide-item-3" class="slide-toggle"/>
-    <label for="slide-item-3"><p class="icon">★</p><span><a href="indicatoaresimarcaje.html">Indicatoare si marcaje</a></span></label>
+<!-- Item 4 -->
+  <input type="radio" name="slideItem" id="slide-item-4" class="slide-toggle"/>
+  <label for="slide-item-4"><p class="icon">★</p><span><a href="../html/Quizez.html">Chestionare DRPCIV </a></span></label>
 
-    <!-- Item 4 -->
-    <input type="radio" name="slideItem" id="slide-item-4" class="slide-toggle"/>
-    <label for="slide-item-4"><p class="icon">★</p><span><a href="chestionare.html">Chestionare DRPCIV </a></span></label>
+  <!-- Item 5 -->
+  <input type="radio" name="slideItem" id="slide-item-5" class="slide-toggle"/>
+  <label for="slide-item-5"><p class="icon">★</p><span><a href="../html/chestionare.html">Categorii permis  </a></span></label>
+
+  <!-- Item 6 -->
+  <input type="radio" name="slideItem" id="slide-item-6" class="slide-toggle"/>
+  <label for="slide-item-6"><p class="icon">★</p><span><a href="../html/Leaderboard.html">Leader board </a></span></label>
 
     <div class="clear"></div>
 
