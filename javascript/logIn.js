347368756de2c884a1bfca55e915b0f30c676797
gabriela0neagu <gabriela0neagu@users.noreply.github.com>
2020-06-09 01:48:18 +0300

    unit test- try

--- javascript/logIn.js
+++ javascript/logIn.js
@@ -1,3 +1,30 @@
 homeBtn = document.getElementById('homeBtn') 
 
 homeBtn.onclick = function(){window.document.location = '../html/Trast.html'}
+
+
+function checkLogin()
+{
+	let errors= 0
+	if (window.XMLHttpRequest) {
+            xmlhttp = new XMLHttpRequest()
+        } else {
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP")
+        }
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4) {
+              errors = this.responseText
+            }
+        }
+        xmlhttp.open("GET","../php/getErrors.php", false)
+        xmlhttp.send()
+        return errors
+}
+
+
+let errors = checkLogin()
+if(errors == 1)
+{
+	alert("Username sau parola invalida");
+}
+
