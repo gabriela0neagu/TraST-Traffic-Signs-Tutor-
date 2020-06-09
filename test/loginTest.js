e788dbf8a19de90bdcc4b65f48c62d2d38a8599d
gabriela0neagu <gabriela0neagu@users.noreply.github.com>
2020-06-09 09:40:14 +0300

    unit testing updates
    + responsiveness modifications

---
+++ test/loginTest.js
@@ -0,0 +1,13 @@
+var assert = require('assert')
+
+it('should let you log in', function () {
+  browser.url('http://localhost/test/html/LogIn.html');
+  browser.setValue('input[name="username"]', 'Gabi');
+  browser.setValue('input[name="password"]', '0000');
+  browser.click('.button=login-btn');
+
+  const pageUrl = browser.getUrl();
+  assert.notEqual(pageUrl, 'http://localhost/test/html/LogIn.html');
+  assert.equal(pageUrl, 'http://localhost/test/html/Profile.html');
+})
+
